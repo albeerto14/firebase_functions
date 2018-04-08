6b933c003fb57ff093240b499cad30b41336864b
Carlos Márquez <carlos_marquez_sanchez@hotmail.com>
2018-04-08 12:15:31 +0200

    Version Nueva BBDD

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,8 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.updateScoreSpectator = functions.database.ref('/Programas/{gala}/PuntuacionesCantantes/{cantante}')
+
+exports.updateScoreSpectator2 = functions.database.ref('/Programas/{gala}/PuntuacionesCantantes/{cantante}')
     .onWrite(event => {
         console.log(event.params.cantante);
         admin.database().ref('/Espectadores')
@@ -12,7 +13,7 @@ exports.updateScoreSpectator = functions.database.ref('/Programas/{gala}/Puntuac
             .on("child_added", snapshot => {
                 console.log(snapshot.val().correo);
                 return snapshot.ref.update({
-                    puntuacion: (snapshot.val().puntuacion + event.data.val())
+                    puntuacion: (parseInt(snapshot.val().puntuacion) + parseInt(event.data.val()))
                 });
             });
         return admin.database().ref('/Espectadores')
@@ -20,32 +21,32 @@ exports.updateScoreSpectator = functions.database.ref('/Programas/{gala}/Puntuac
             .on("child_added", snapshot => {
                 console.log(snapshot.val().correo);
                 return snapshot.ref.update({
-                    puntuacion: (snapshot.val().puntuacion + event.data.val())
+                    puntuacion: (parseInt(snapshot.val().puntuacion) + parseInt(event.data.val()))
                 });
             });
     });
+
+exports.updateScoreSpectatorPair = functions.database.ref('/Programas/{gala}/PuntuacionesDuetos/{idDueto}')
+    .onWrite(event => {
+        return admin.database().ref('/Espectadores')
+            .orderByChild('dueto').equalTo(event.params.idDueto)
+            .on("child_added", snapshot => {
+                return snapshot.ref.update({
+                    puntuacion: (parseInt(snapshot.val().puntuacion) + parseInt(event.data.val()))
+                });
+            });
+    });
+
 
 exports.updateScoreSinger = functions.database.ref('/Programas/{gala}/PuntuacionesCantantes/{cantante}')
     .onWrite(event => {
         return admin.database().ref('/Cantantes/' + event.params.cantante)
             .on("value", snapshot => {
                 return snapshot.ref.update({
-                    puntuacionTotal: (snapshot.val().puntuacionTotal + event.data.val())
+                    puntuacionTotal: (parseInt(snapshot.val().puntuacionTotal) + parseInt(event.data.val()))
                 });
             });
     });
-
-exports.updateScorePair = functions.database.ref('/Programas/{gala}/PuntuacionesDuetos/{idDueto}/Total')
-    .onWrite(event => {
-        return admin.database().ref('/Espectadores')
-            .orderByChild('dueto').equalTo(event.params.idDueto)
-            .on("child_added", snapshot => {
-                return snapshot.ref.update({
-                    puntuacionDueto: (snapshot.val().puntuacionDueto + event.data.val())
-                });
-            });
-    });
-
 
 exports.updateRanking = functions.https.onRequest((req, res) => {
     var contador = 0;
@@ -53,10 +54,13 @@ exports.updateRanking = functions.https.onRequest((req, res) => {
         on("value", snapshot => {
             contador = (snapshot.numChildren());
         });
-
     admin.database().ref('/Espectadores').orderByChild('puntuacion').on("child_added", snapshot => {
         var json = {};
-        json[contador] = snapshot.val().email;
+        if(snapshot.val().privacidad === "privado"){
+            json[contador] = "Anonimo   -   "  + String(snapshot.val().puntuacion);
+        }else {
+            json[contador] = snapshot.val().email + "   -   "  + String(snapshot.val().puntuacion);
+        }
         contador--;
         return snapshot.ref.parent.parent.child("Ranking").update(json);
     });
